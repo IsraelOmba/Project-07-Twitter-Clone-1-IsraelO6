4389717b59dfe63eaf80fc1911783b45e6e0208d
israel omba <israelomba1960@gmail.com>
2023-12-05 18:19:01 +0000

    implementation of the home

--- src/components/tweets/tweet/tweet-content/tweet-body/tweet-image.jsx
+++ src/components/tweets/tweet/tweet-content/tweet-body/tweet-image.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-function TweetImage({TweetImage}) {
-  if (TweetImage) {
+function TweetImage({tweetImage}) {
+  if (tweetImage) {
     return (
       <div className='tweet-image'>
-        <img src={TweetImage} alt='label'/>
+        <img src={tweetImage} alt='label'/>
       </div>
     );
-  } else {
+  } 
+  
+  else {
     return null;
   }
 }
 
 export default TweetImage;
+
+// import React from 'react';
+
+// function ImageDisplay() {
+//   return (
+//     <div>
+//       <img src="./image3.png" alt="Image 3" />
+//     </div>
+//   );
+// }
+
+// export default ImageDisplay;
